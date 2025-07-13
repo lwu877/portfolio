da4c33c43804d6d03c942cdda533f28a10c6b355
lwu877 <lex@lexwu.com>
2025-07-13 00:41:04 -0400

    Added Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,18 +7,24 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lexwu.com',
   experimental: {},
   integrations: [tailwind(), compress(), pagefind(), mdx(), sitemap()],
+
   build: {
     format: "file",
   },
+
   redirects: {
     "/work/[id]": "/projects/[id]",
     "/work/curtain-lorimer/[id]": "/projects/[id]",
     "/blog": "https://blog.lexwu.com",
     "/bio": "https://bio.lexwu.com",
-  }
+  },
+
+  adapter: cloudflare()
 });
