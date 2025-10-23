46ff5676ebee064a6287e7b444a19101d9f14f89
lwu877 <lex@lexwu.com>
2025-10-23 17:29:18 -0400

    Added Swup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import pagefind from 'astro-pagefind';
-
+import swup from '@swup/astro'
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
@@ -13,7 +13,16 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   site: 'https://lexwu.com',
   experimental: {},
-  integrations: [tailwind(), compress(), pagefind(), mdx(), sitemap()],
+  integrations: [tailwind(), compress(), pagefind(), mdx(), sitemap(), swup({
+    theme: 'fade',
+      cache: true,
+      preload: {
+        visible: true
+      },
+      morph: ['header', 'footer'],
+      progress: true,
+      smoothScrolling: true,
+  })],
 
   build: {
     format: "file",
